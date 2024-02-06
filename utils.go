5e14e0c8a04bfa379d14361cd577bcc2f8ee615d
Stacey Brogden <toad@brothertoad.net>
2024-02-06 09:56:42 -0800

    Support hours in duration.

--- utils.go
+++ utils.go
@@ -19,11 +19,20 @@ func readFile(path string) []byte {
 }
 
 func setDuration(duration float64, m TagMap) {
-  // Round to nearest integer, make it a string, convert to mm:ss.
+  // Round to nearest integer, make it a string, convert to [hh:]mm:ss.
   totalSeconds := int(math.Round(duration))
+  hours := 0
   minutes := totalSeconds / 60
   seconds := totalSeconds % 60
-  m[DurationKey] = fmt.Sprintf("%d:%02d", minutes, seconds)
+  for ; minutes > 59 ; {
+    hours++
+    minutes = minutes - 60
+  }
+  if hours > 0 {
+    m[DurationKey] = fmt.Sprintf("%d:%02d:%02d", hours, minutes, seconds)
+  } else {
+    m[DurationKey] = fmt.Sprintf("%d:%02d", minutes, seconds)
+  }
 }
 
 func setMimeAndExtension(mime string, extension string, m TagMap) {
